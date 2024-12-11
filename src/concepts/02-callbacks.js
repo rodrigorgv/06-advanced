9b682227c1a62a25b53e96689891574cf4848ccc
Rodrigo Gonzalez <rodrigo.emilianogv@gmail.com>
2024-12-11 14:24:00 -0600

    se trabaja seccion 133 referente a los callback hell, con un pequeño ejemplo en 02-callbacks

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -4,16 +4,23 @@ import {heroes} from '../data/heroes';
  * @param {HTMLDivElement} element 
  */
 export const callbacksComponent = (element)=>{
-    const id = '5d86371fd55e2e2a30fe1cc4x';
+    const id1 = '5d86371fd55e2e2a30fe1cc4';
+    const id2 = '5d86371f97c29d020f1e1f6d';
     console.log('callbacksComponent');
-    findHero(id,(error, heroe) =>{
+    findHero(id1,(error, heroe) =>{
         //--MANEJO DE ERRORES EN CALLBACKS #1
         //element.innerHTML = heroe?.name || 'No hay heroe';
         if (error){
             element.innerHTML = error;    
         }
 
-        element.innerHTML = heroe.name;
+        findHero(id2, (error, hero2) =>{
+            if (error){
+                element.innerHTML = error;    
+            }
+            element.innerHTML = `${heroe.name} / ${hero2.name}`;
+        })
+
     });
 }
 /**
