0a76cbc4ce92b83ba41908442afb655ed24f1b64
Rodrigo Gonzalez <rodrigo.emilianogv@gmail.com>
2025-01-06 14:41:30 -0600

    se completa 05-async, con manejo de errores en funciones asincronas

--- src/concepts/05-async.js
+++ src/concepts/05-async.js
@@ -6,13 +6,14 @@ import {heroes} from '../data/heroes'
 export const asyncComponent = (element)=>{
 
     const id1 = '5d86371f2343e37870b91ef1';
+    console.log('Inicio de componente');
     
-
-    //console.log(findHero(id1));
 
     findHero(id1)
         .then(name => element.innerHTML = name)
         .catch(error => element.innerHTML = error)
+
+    console.log('Fin de componente');
 }
 
 /**
@@ -22,5 +23,7 @@ export const asyncComponent = (element)=>{
  */
 const findHero = async (id) =>{
     const hero = heroes.find(hero => hero.id === id);
+    if (!hero)
+        throw `Hero with id ${id} not found`;
     return hero.name;
 }
